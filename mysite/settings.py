b461545c382d3d1424d6c1e751283695b98da574
Bibek-Dahal <bibekdahal479@gmail.com>
2022-08-07 00:23:32 +0545

    debugging

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['samrakxad.herokuapp.com','127.0.0.1','0.0.0.0','localhost']
 
